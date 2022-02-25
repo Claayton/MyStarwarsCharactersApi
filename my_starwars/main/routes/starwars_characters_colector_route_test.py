9749fa23876db1aefb47d419cb53cd33873eca8c
Clayton Garcia <claaytongarcia@gmail.com>
2022-02-25 00:49:22 -0300

    feat: Implementado nova rota que mostra todos os personagens de starwars e suas caracteristicas principais

---
+++ my_starwars/main/routes/starwars_characters_colector_route_test.py
@@ -0,0 +1,20 @@
+"""Testes para a rota get_starwars_characters"""
+from fastapi.testclient import TestClient
+from .starwars_characters_colector_route import colector
+
+client = TestClient(colector)
+
+
+def test_get_starwars_characters():
+    """Testando a rota get_starwars_characters"""
+
+    url = "api/colector/characters/"
+
+    response = client.get(url=url)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert isinstance(response.json()["data"], list)
+    assert isinstance(response.json()["data"][0], dict)
+    assert "name" in response.json()["data"][0]
+    assert "eye_color" in response.json()["data"][0]
