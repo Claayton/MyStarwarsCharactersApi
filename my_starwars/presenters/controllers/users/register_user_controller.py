4724418fb26fa0d5bc98cda0c23b538b75af9653
Clayton Garcia <claaytongarcia@gmail.com>
2022-02-26 20:10:30 -0300

    chore: Implementado o metodo privado __format_response, para formatar a resposta do metodo handler + Algumas mudanças nos retornos do metodo

--- my_starwars/presenters/controllers/users/register_user_controller.py
+++ my_starwars/presenters/controllers/users/register_user_controller.py
@@ -4,6 +4,7 @@ from my_starwars.presenters.helpers import HttpRequest, HttpResponse
 from my_starwars.domain.usecases import RegisterUserInterface as RegisterUser
 from my_starwars.presenters.interfaces import ControllerInterface
 from my_starwars.errors import HttpBadRequestError
+from my_starwars.domain.models import User
 
 
 class RegisterUserController(ControllerInterface):
@@ -28,15 +29,32 @@ class RegisterUserController(ControllerInterface):
             ):
                 name = http_request.body["name"]
                 email = http_request.body["email"]
-                password = http_request.body["password"]
+                password = str(http_request.body["password"])
 
                 response = self.__usecase.register(
                     name=name, email=email, password=password
                 )
+                formated_response = self.__format_response(response["data"])
 
-                return HttpResponse(status_code=201, body=response["data"])
+                return formated_response
 
         raise HttpBadRequestError(
             message="Esta rota necessita dos seguintes parametros:\
                 'name: str', 'email: str', 'password: str'"
         )
+
+    @classmethod
+    def __format_response(cls, response_method: Type[User]) -> HttpResponse:
+        """Formatando a resposta"""
+
+        response = {
+            "message": "Usuario registrado com sucesso!",
+            "data": {
+                "id": response_method.id,
+                "name": response_method.name,
+                "email": response_method.email,
+                "password": "Não mostramos isso aqui!",
+            },
+        }
+
+        return HttpResponse(status_code=201, body=response)
