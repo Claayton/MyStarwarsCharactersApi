3c5485a0f44867f4a52e9a312400699c53965e22
Clayton Garcia <claaytongarcia@gmail.com>
2022-02-27 02:03:04 -0300

    feat: Implementado a rota get_user + Testes

--- my_starwars/main/routes/users_routes.py
+++ my_starwars/main/routes/users_routes.py
@@ -3,10 +3,27 @@ from fastapi import APIRouter, Request as RequestFastApi
 from fastapi.responses import JSONResponse
 from my_starwars.main.adapters import request_adapter
 from my_starwars.presenters.errors import handler_errors
-from my_starwars.main.composers import register_user_composer
-from my_starwars.validators import register_user_validator
+from my_starwars.main.composers import register_user_composer, get_user_composer
+from my_starwars.validators import register_user_validator, get_user_validator
 
 users = APIRouter(prefix="/api/users")
+
+
+@users.get("/")
+async def get_user(request: RequestFastApi):
+    """Rota para buscar usuarios registrados no sistema"""
+
+    response = None
+
+    try:
+        await get_user_validator(request)
+        controller = get_user_composer()
+        response = await request_adapter(request, controller.handler)
+
+    except Exception as error:  # pylint: disable=W0703
+        response = handler_errors(error)
+
+    return JSONResponse(status_code=response.status_code, content=response.body)
 
 
 @users.post("/")
--- my_starwars/main/routes/users_routes_test.py
+++ my_starwars/main/routes/users_routes_test.py
@@ -1,10 +1,134 @@
-"""Testes para a rota register_user"""
+"""Testes para as rotas de users"""
 from fastapi.testclient import TestClient
 from faker import Faker
+from my_starwars.infra.database.config import DataBaseConnectionHandler
+from my_starwars.config import CONNECTION_STRING
 from .users_routes import users
 
 fake = Faker()
 client = TestClient(users)
+data_base_connection_handler = DataBaseConnectionHandler(CONNECTION_STRING)
+
+
+def test_get_user_with_name_param():
+    """
+    Testando a rota get_users.
+    Utilizando um valor valido como parametro para name.
+    """
+
+    user_id = fake.random_number(digits=6)
+    name = fake.name()
+    email = f"{fake.word()}@test.com"
+    password = fake.word()
+
+    url = "/api/users/"
+    params = {"name": name}
+
+    engine = data_base_connection_handler.get_engine()
+    engine.execute(
+        f"INSERT INTO users (id, name, email, password_hash)\
+            VALUES ('{user_id}', '{name}', '{email}', '{password}');"
+    )
+
+    response = client.get(url=url, params=params)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert isinstance(response.json()["data"], dict)
+    assert "id" in response.json()["data"]
+    assert "name" in response.json()["data"]
+
+    engine.execute(f"DELETE FROM users WHERE name='{name}';")
+
+
+def test_get_user_with_email_param():
+    """
+    Testando a rota get_users.
+    Utilizando um valor valido como parametro para email.
+    """
+
+    user_id = fake.random_number(digits=6)
+    name = fake.name()
+    email = f"{fake.word()}@test.com"
+    password = fake.word()
+
+    url = "/api/users/"
+    params = {"email": email}
+
+    engine = data_base_connection_handler.get_engine()
+    engine.execute(
+        f"INSERT INTO users (id, name, email, password_hash)\
+            VALUES ('{user_id}', '{name}', '{email}', '{password}');"
+    )
+
+    response = client.get(url=url, params=params)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert isinstance(response.json()["data"], dict)
+    assert "id" in response.json()["data"]
+    assert "name" in response.json()["data"]
+
+    engine.execute(f"DELETE FROM users WHERE email='{email}';")
+
+
+def test_get_user_with_user_id_param():
+    """
+    Testando a rota get_users.
+    Utilizando um valor valido como parametro para user_id.
+    """
+
+    user_id = fake.random_number(digits=6)
+    name = fake.name()
+    email = f"{fake.word()}@test.com"
+    password = fake.word()
+
+    url = "/api/users/"
+    params = {"user_id": user_id}
+
+    engine = data_base_connection_handler.get_engine()
+    engine.execute(
+        f"INSERT INTO users (id, name, email, password_hash)\
+            VALUES ('{user_id}', '{name}', '{email}', '{password}');"
+    )
+
+    response = client.get(url=url, params=params)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert isinstance(response.json()["data"], dict)
+    assert "id" in response.json()["data"]
+    assert "name" in response.json()["data"]
+
+    engine.execute(f"DELETE FROM users WHERE id='{user_id}';")
+
+
+def test_get_user_error400_without_query_params():
+    """
+    Testando o erro 400 na rota get_users.
+    Sem utilizar nenhum parametro.
+    """
+
+    user_id = fake.random_number(digits=6)
+    name = fake.name()
+    email = f"{fake.word()}@test.com"
+    password = fake.word()
+
+    url = "/api/users/"
+    params = {}
+
+    engine = data_base_connection_handler.get_engine()
+    engine.execute(
+        f"INSERT INTO users (id, name, email, password_hash)\
+            VALUES ('{user_id}', '{name}', '{email}', '{password}');"
+    )
+
+    response = client.get(url=url, params=params)
+
+    assert response.status_code == 400
+    assert "error" in response.json()
+
+    engine.execute(f"DELETE FROM users WHERE id='{user_id}';")
 
 
 def test_register_user():
@@ -17,6 +141,8 @@ def test_register_user():
         "password": fake.word(),
     }
 
+    engine = data_base_connection_handler.get_engine()
+
     response = client.post(url=url, json=data)
 
     assert response.status_code == 201
@@ -27,10 +153,13 @@ def test_register_user():
     assert "email" in response.json()["data"]
     assert "password" in response.json()["data"]
 
+    name = data["name"]
+    engine.execute(f"DELETE FROM users WHERE name='{name}';")
 
-def test_register_user_error402_with_invalid_name_param():
+
+def test_register_user_error422_with_invalid_name_param():
     """
-    Testando o erro 402 - Unprocessable Entity na rota register_user.
+    Testando o erro 422 - Unprocessable Entity na rota register_user.
     Utilizando um valor invalido para o parametro 'name'.
     """
 
@@ -43,13 +172,13 @@ def test_register_user_error402_with_invalid_name_param():
 
     response = client.post(url=url, json=data)
 
-    assert response.status_code == 402
+    assert response.status_code == 422
     assert "error" in response.json()
 
 
-def test_register_user_error402_with_invalid_email_param():
+def test_register_user_error422_with_invalid_email_param():
     """
-    Testando o erro 402 - Unprocessable Entity na rota register_user.
+    Testando o erro 422 - Unprocessable Entity na rota register_user.
     Utilizando um valor invalido para o parametro 'email'.
     """
 
@@ -58,13 +187,13 @@ def test_register_user_error402_with_invalid_email_param():
 
     response = client.post(url=url, json=data)
 
-    assert response.status_code == 402
+    assert response.status_code == 422
     assert "error" in response.json()
 
 
-def test_register_user_error402_without_params():
+def test_register_user_error400_without_body_params():
     """
-    Testando o erro 402 - Unprocessable Entity na rota register_user.
+    Testando o erro 400 - Bad Request na rota register_user.
     Utilizando um valor invalido para o parametro 'name'.
     """
 
