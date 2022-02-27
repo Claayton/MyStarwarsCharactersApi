19cdb6b2db8479549586fa0f008ebd0d297f0446
Clayton Garcia <claaytongarcia@gmail.com>
2022-02-27 03:09:19 -0300

    feat: Implementado roda get_users, para realizar a busca de todos os usuarios do sistema

--- my_starwars/main/routes/users_routes_test.py
+++ my_starwars/main/routes/users_routes_test.py
@@ -10,6 +10,33 @@ client = TestClient(users)
 data_base_connection_handler = DataBaseConnectionHandler(CONNECTION_STRING)
 
 
+def test_get_users():
+    """Testando a rota get_users"""
+
+    user_id = fake.random_number(digits=6)
+    name = fake.name()
+    email = f"{fake.word()}@test.com"
+    password = fake.word()
+
+    url = "/api/users/all/"
+
+    engine = data_base_connection_handler.get_engine()
+    engine.execute(
+        f"INSERT INTO users (id, name, email, password_hash)\
+            VALUES ('{user_id}', '{name}', '{email}', '{password}');"
+    )
+
+    response = client.get(url=url)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert isinstance(response.json()["data"], list)
+    assert "id" in response.json()["data"][0]
+    assert "name" in response.json()["data"][0]
+
+    engine.execute(f"DELETE FROM users WHERE name='{name}';")
+
+
 def test_get_user_with_name_param():
     """
     Testando a rota get_users.
