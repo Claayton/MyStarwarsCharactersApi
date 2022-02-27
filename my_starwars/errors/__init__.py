823bfd7250c698ef65354c2d08628130a3e5d8a9
Clayton Garcia <claaytongarcia@gmail.com>
2022-02-27 04:24:27 -0300

    feat: Implementado erro401 Unauthorized

--- my_starwars/errors/__init__.py
+++ my_starwars/errors/__init__.py
@@ -1,4 +1,5 @@
 """Arquivo de inicializaçao do modulo errors"""
 from .http_request_error import HttpRequestError
 from .http_error400 import HttpBadRequestError
+from .http_error401 import HttpUnauthorized
 from .http_error422 import HttpUnprocessableEntity
--- my_starwars/errors/http_error422.py
+++ my_starwars/errors/http_error422.py
@@ -4,8 +4,8 @@
 class HttpUnprocessableEntity(Exception):
     """HttpError 422 - Unprocessable Entity!"""
 
-    def __init__(self, message: str = " Unprocessable Entity!") -> None:
+    def __init__(self, message: str = "Unprocessable Entity!") -> None:
         super().__init__(message)
         self.message = message
-        self.name = " UnprocessableEntity!Error"
+        self.name = " UnprocessableEntityError"
         self.status_code = 422
