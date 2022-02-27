f15a5160c91e7067b8535f22ada72df58b0311bb
Clayton Garcia <claaytongarcia@gmail.com>
2022-02-27 19:07:04 -0300

    chore: Atualizado o request_adapter que nao estava recebendo os headers

--- my_starwars/main/adapters/request_adapter.py
+++ my_starwars/main/adapters/request_adapter.py
@@ -14,7 +14,9 @@ async def request_adapter(request: RequestFastApi, callback: Callable):
     except:  # pylint: disable=W0702
         pass
 
-    http_request = HttpRequest(body=body, query=request.query_params)
+    http_request = HttpRequest(
+        header=request.headers, body=body, query=request.query_params
+    )
 
     http_response = callback(http_request)
     return http_response
