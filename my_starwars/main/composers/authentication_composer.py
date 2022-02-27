4d06418cbc9d483dc0b69ee9ddff2058b4b31918
Clayton Garcia <claaytongarcia@gmail.com>
2022-02-27 12:34:22 -0300

    feat: Implementado composer para o caso de uso Authentication

---
+++ my_starwars/main/composers/authentication_composer.py
@@ -0,0 +1,18 @@
+"""Arquivo para montar o caso de uso Authentication"""
+from my_starwars.infra.database.repo import UserRepo
+from my_starwars.data.auth import Authentication
+from my_starwars.data.users import HashPassword, GetUser
+from my_starwars.presenters.controllers.auth import AuthenticationController
+from my_starwars.config import CONNECTION_STRING
+
+
+def authentication_composer():
+    """Montagem do caso de uso Authentication"""
+
+    infra = UserRepo(CONNECTION_STRING)
+    get_user = GetUser(infra)
+    hash_password = HashPassword()
+    usecase = Authentication(get_user, hash_password)
+    controller = AuthenticationController(usecase)
+
+    return controller
---
+++ my_starwars/presenters/controllers/auth/__init__.py
@@ -0,0 +1,2 @@
+"""Arquivo de inicializaçao do modulo auth"""
+from .authentication_controller import AuthenticationController
