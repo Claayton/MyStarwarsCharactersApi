0192fb2014d007964e85d5f340edb8cfde37ea5c
Clayton Garcia <claaytongarcia@gmail.com>
2022-03-01 02:58:08 -0300

    feat: Implementado nova rota para buscar os personagens que estao cadastrados no banco de dados

--- my_starwars/main/routes/characters_routes.py
+++ my_starwars/main/routes/characters_routes.py
@@ -6,13 +6,31 @@ from my_starwars.presenters.errors import handler_errors
 from my_starwars.main.composers import (
     starwars_characters_colector_composer,
     register_character_composer,
+    get_character_composer,
 )
 
 characters = APIRouter(prefix="/api/characters")
 
 
-@characters.get("/direct/")
+@characters.get("/")
 async def get_starwars_characters(request: RequestFastApi):
+    """Rota para buscar os personagens de starwars e seus dados"""
+
+    response = None
+
+    try:
+
+        controller = get_character_composer()
+        response = await request_adapter(request, controller.handler)
+
+    except Exception as error:  # pylint: disable=W0703
+        response = handler_errors(error)
+
+    return JSONResponse(status_code=response.status_code, content=response.body)
+
+
+@characters.get("/direct/")
+async def get_starwars_characters_direct(request: RequestFastApi):
     """Rota para buscar os principais dados dos personagens de starwars"""
 
     response = None
--- my_starwars/main/routes/characters_routes_test.py
+++ my_starwars/main/routes/characters_routes_test.py
@@ -1,12 +1,29 @@
 """Testes para a rota get_starwars_characters"""
 from fastapi.testclient import TestClient
+from faker import Faker
 from .characters_routes import characters
 
 client = TestClient(characters)
+fake = Faker()
 
 
 def test_get_starwars_characters():
-    """Testando a rota get_starwars_characters"""
+    """Testando a rota _get_starwars_characters"""
+
+    url = "api/characters/"
+
+    response = client.get(url=url)
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    assert isinstance(response.json()["data"], list)
+    assert isinstance(response.json()["data"][0], dict)
+    assert "name" in response.json()["data"][0]
+    assert "eye_color" in response.json()["data"][0]
+
+
+def test_get_starwars_characters_direct():
+    """Testando a rota get_starwars_characters get_starwars_characters_direct"""
 
     url = "api/characters/direct/"
 
