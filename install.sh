20db4e2dd1e5edb8ed50cc932ded5b8f90aed052
Clayton Garcia <claaytongarcia@gmail.com>
2022-03-03 20:42:59 -0300

    build: Adicionado string de conexao para o db de testes

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ pip3 install -r requirements.txt;
 pre-commit install;
 echo "SEARCH_URL=https://swapi.dev/api/people/
 CONNECTION_STRING=sqlite:///storage.db
-SECRET_KEY=mecontratapiazada123" > .env;
+CONNECTION_STRING_TEST=sqlite:///storage_test.db
+SECRET_KEY=mecontratapiazada123" > .env
 python3 run.py;
